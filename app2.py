dd1d5adeebaad7eb0ef25ad6377d86b9e282172e
saini1616 <sainiguharoy47@gmail.com>
2024-09-20 00:43:36 +0530

    Add files via upload
    My first Commit

---
+++ app2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Streamlit does not use routes, everything is executed in order
+st.title("Home Page")
+
+# Display "Hello, World!" on the main page
+st.write("Hello, World!")
+
+
